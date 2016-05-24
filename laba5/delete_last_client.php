fbd4811e81d9b6bd7e6b58049402ba960108e400
Stepasha666 <ned6@bk.ru>
2016-05-24 18:55:53 +0500

    Create delete_last_client.php

---
+++ laba5/delete_last_client.php
@@ -0,0 +1,14 @@
+<?php
+require  'myconnection.php';
+require  'client.php';
+session_start();
+$connect = new myconnection('web_techn', 'Stepa', '123', 'client', 'localhost');
+$datas = Client::delete_last_client();
+$back = $_SERVER['HTTP_REFERER']; 
+		echo "
+		<html>
+  		<head>
+  		<meta http-equiv='Refresh' content='0; URL=".$_SERVER['HTTP_REFERER']."'>
+  		</head>
+		</html>";	
+?>
