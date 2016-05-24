139d8db0864a6a4f60280d88fd1230304722dbc2
Stepasha666 <ned6@bk.ru>
2016-05-24 18:56:34 +0500

    Create myconnection.php

---
+++ laba5/myconnection.php
@@ -0,0 +1,45 @@
+<?php
+	
+class myconnection
+{
+	private static  $dbname = '';//'web_techn';
+	private static  $user = '';//'stepa';
+	private static  $pass = '';//'123';
+	private static  $table = '';//'client';
+	private static  $server = '';//localhost
+	
+	function __construct($name, $usr, $pw, $tbl, $serv) 
+	{
+       self::$dbname = $name;
+	   self::$user = $usr;
+	   self::$pass = $pw;
+	   self::$table = $tbl;
+	   self::$server = $serv;	  
+	}
+	static function get_dbname()
+	{
+		return self::$dbname;
+	}
+	
+	static function get_user()
+	{
+		return self::$user;
+	}
+	
+	static function get_pass()
+	{
+		return self::$pass;
+	}
+	
+	static function get_table()
+	{
+		return self::$table;
+	}
+	
+	static function get_server()
+	{
+		return self::$server;
+	}
+	
+}
+?>
